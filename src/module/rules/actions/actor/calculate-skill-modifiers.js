a8a91007e321640d4f6962556da1f0cda6eac71f
Ian Bennett <34078802+ian612@users.noreply.github.com>
2025-09-26 11:50:51 -0400

    rewrite skill modifier calculation for clarity

--- src/module/rules/actions/actor/calculate-skill-modifiers.js
+++ src/module/rules/actions/actor/calculate-skill-modifiers.js
@@ -5,68 +5,67 @@ export default function(engine) {
         const skills = fact.data.skills;
         const modifiers = fact.modifiers;
 
-        const addModifier = (bonus, data, item, localizationKey) => {
-            if (item.calculatedMods) {
-                item.calculatedMods.push({mod: bonus.modifier, bonus: bonus});
-            } else {
-                item.calculatedMods = [{mod: bonus.modifier, bonus: bonus}];
-            }
-            const computedBonus = bonus.max || 0;
-
-            if (computedBonus !== 0 && localizationKey) {
-                item.tooltip.push(game.i18n.format(localizationKey, {
-                    type: game.i18n.format(`SFRPG.ModifierType${bonus.type.capitalize()}`),
-                    mod: computedBonus.signedString(),
-                    source: bonus.name
-                }));
-            }
-
-            return computedBonus;
-        };
-
         const filteredMods = modifiers.filter(mod => {
             return (mod.enabled || mod.modifierType === "formula") && [SFRPGEffectType.ABILITY_SKILLS, SFRPGEffectType.SKILL, SFRPGEffectType.ALL_SKILLS].includes(mod.effectType);
         });
 
         // Skills
         for (const [skl, skill] of Object.entries(skills)) {
-            skill.rolledMods = null;
+            skill.rolledMods = [];
             const mods = context.parameters.stackModifiers.process(filteredMods.filter(mod => {
-                // temporary workaround to fix modifiers with mod "0" if the situational mod is higher.
-                if (mod.modifierType === SFRPGModifierType.FORMULA && ((mod.effectType === SFRPGEffectType.ALL_SKILLS)
+                const isRelevantMod = (mod.effectType === SFRPGEffectType.ALL_SKILLS)
                     || (mod.effectType === SFRPGEffectType.SKILL && skl === mod.valueAffected)
-                    || (mod.effectType === SFRPGEffectType.ABILITY_SKILLS && skill.ability === mod.valueAffected))) {
-                    if (skill.rolledMods) {
-                        skill.rolledMods.push({mod: mod.modifier, bonus: mod});
-                    } else {
-                        skill.rolledMods = [{mod: mod.modifier, bonus: mod}];
-                    }
-                    return false;
-                }
-                if (mod.effectType === SFRPGEffectType.ALL_SKILLS) return true;
-                else if (mod.effectType === SFRPGEffectType.SKILL && skl === mod.valueAffected) return true;
-                else if (mod.effectType === SFRPGEffectType.ABILITY_SKILLS && skill.ability === mod.valueAffected) return true;
+                    || (mod.effectType === SFRPGEffectType.ABILITY_SKILLS && skill.ability === mod.valueAffected);
 
-                return false;
+                // temporary workaround to fix modifiers with mod "0" if the situational mod is higher.
+                if (mod.modifierType === SFRPGModifierType.FORMULA && isRelevantMod) {
+                    skill.rolledMods.push({mod: mod.modifier, bonus: mod});
+                    return false;
+                } else {
+                    return isRelevantMod ? true : false;
+                }
             }), context, {actor: fact.actor});
 
-            const accumulator = Object.entries(mods).reduce((sum, mod) => {
-                if (mod[1] === null || mod[1].length < 1) return sum;
+            const bonus = calculateBonus(mods, fact, skill);
 
-                if ([SFRPGModifierTypes.CIRCUMSTANCE, SFRPGModifierTypes.UNTYPED].includes(mod[0])) {
-                    for (const bonus of mod[1]) {
-                        sum += addModifier(bonus, fact.data, skill, "SFRPG.SkillModifierTooltip");
-                    }
-                } else {
-                    sum += addModifier(mod[1], fact.data, skill, "SFRPG.SkillModifierTooltip");
-                }
-
-                return sum;
-            }, 0);
-
-            skill.mod += accumulator;
+            skill.mod += bonus;
         }
 
         return fact;
     }, { required: ["stackModifiers"], closureParameters: ["stackModifiers"] });
+}
+
+function addModifier(bonus, data, item, localizationKey) {
+    if (item.calculatedMods) {
+        item.calculatedMods.push({mod: bonus.modifier, bonus: bonus});
+    } else {
+        item.calculatedMods = [{mod: bonus.modifier, bonus: bonus}];
+    }
+    const computedBonus = bonus.max || 0;
+
+    if (computedBonus !== 0 && localizationKey) {
+        item.tooltip.push(game.i18n.format(localizationKey, {
+            type: game.i18n.format(`SFRPG.ModifierType${bonus.type.capitalize()}`),
+            mod: computedBonus.signedString(),
+            source: bonus.name
+        }));
+    }
+
+    return computedBonus;
+}
+
+function calculateBonus(mods, fact, skill) {
+    const data = fact.data;
+    let sum = 0;
+    for (let [bonusType, bonuses] of Object.entries(mods)) {
+        if (bonuses === null || bonuses.length === 0) continue;
+        if (![SFRPGModifierTypes.CIRCUMSTANCE, SFRPGModifierTypes.UNTYPED].includes(bonusType)) {
+            bonuses = [bonuses];
+        }
+
+        for (const bonus of bonuses) {
+            sum += addModifier(bonus, data, skill, "SFRPG.SkillModifierTooltip");
+        }
+    }
+    return sum;
 }
